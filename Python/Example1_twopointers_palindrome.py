4cbe34e25f991f8b85a90d290f40ae19ce85fe47
Tuan Vo <hoanhtuan.vo@gmail.com>
2025-05-07 14:42:01 +0700

    added comments

--- Python/Example1_twopointers_palindrome.py
+++ Python/Example1_twopointers_palindrome.py
@@ -1,8 +1,14 @@
 def check_palindrome(s: str):
+    # "Set the starting and ending pointers to the first and last characters of the string."
     left = 0
     right = len(s) - 1
 
-    while(left < right):
+    # "Iterate until the left pointer is less than the right pointer, which means we have checked all characters."
+    #  "If the characters at the left and right pointers are not equal, return False."
+    #  "If they are equal, move the left pointer one step to the right and the right pointer one step to the left."
+    #  "If we reach this point, it means the string is a palindrome, so return True."
+
+    while left < right:
         if s[left] != s[right]:
             return False
         left += 1
